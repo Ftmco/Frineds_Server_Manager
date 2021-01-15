2925cb283a250ff1fddd12a687ade4f0f69fd216
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-15 18:28:50 +0330

    update View

--- FServerManager/ServerManager.WPF/MainWindow.xaml.cs
+++ FServerManager/ServerManager.WPF/MainWindow.xaml.cs
@@ -35,5 +35,13 @@ namespace ServerManager.WPF
                 addButton.ContextMenu.IsOpen = true;
             }
         }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+        }
     }
 }
