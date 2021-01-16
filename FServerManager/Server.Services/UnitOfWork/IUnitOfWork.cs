607548b1481bdfe61982f2ba1c9db458eb3242e1
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-16 20:42:12 +0330

    Update version insert packages

---
+++ FServerManager/Server.Services/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Services.UnitOfWork
+{
+    public interface IUnitOfWork<TContext> where TContext : class
+    {
+        #region __Dependency__
+
+        private readonly 
+
+        #endregion
+    }
+}
