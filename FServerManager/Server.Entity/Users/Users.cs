ecc7be0ac33be6034b75c2778e1c080ca62e53cf
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-20 23:39:42 +0330

    update entity

--- FServerManager/Server.Entity/Users/Users.cs
+++ FServerManager/Server.Entity/Users/Users.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Server.Entity.Users
+public record Users
 {
-    class Users
-    {
-    }
+    public Guid UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ActiveCode { get; set; }
+    public DateTime ActiveDate { get; set; }
+    public bool IsActive { get; set; }
 }
