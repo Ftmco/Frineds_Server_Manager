8d67cca29fc9bb5055a68c32cdb3a528bd11240f
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-03-11 00:31:26 +0330

    Insert Socket Service

---
+++ FServerManager/InterfaceServer.Socket/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceServer.Socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
