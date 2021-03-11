1928a7d1deecbded182cd14ea65226957973632f
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-03-11 14:53:08 +0330

    change mobile app

---
+++ FServerManager/Src/Client/Mobile/FServerManager.Mobile/FServerManager.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FServerManager.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FServerManager.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ FServerManager/Src/Client/Mobile/FServerManager.Mobile/FServerManager.Mobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FServerManager.Mobile.Models;
+using FServerManager.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FServerManager.Mobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
